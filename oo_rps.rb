4faa7b4c77e95c77dbed06e63a7715efaa18b98d
danielboyle <danielboyle111@gmail.com>
2015-12-09 16:25:34 -0500

    Add score functionality

--- oo_rps.rb
+++ oo_rps.rb
@@ -82,10 +82,15 @@ class RPSGame
   def initialize
     @human = Human.new
     @computer = Computer.new
+
+    @player_wins = 0
+    @computer_wins = 0
+    @ties = 0
   end
 
   def display_welcome_message
     puts "Welcome to Rock, Paper, Scissors!"
+    puts "First to 10 wins, WINS!!!"
   end
 
   def display_goodbye_message
@@ -100,11 +105,20 @@ class RPSGame
   def display_winner
     if human.move > computer.move
       puts "#{human.name} won!"
+      @player_wins += 1
     elsif human.move < computer.move
       puts "#{computer.name} won!"
+      @computer_wins += 1
     else
       puts "It's a tie!"
+      @ties += 1
     end
+  end
+
+  def display_score
+    puts
+    puts "Score // #{human.name}: #{@player_wins} / #{computer.name}: #{@computer_wins} / Ties: #{@ties}"
+    puts
   end
 
   def play_again?
@@ -120,6 +134,10 @@ class RPSGame
     return true if answer.downcase == 'y'
   end
 
+  def has_10_wins?
+    @player_wins == 10 || @computer_wins == 10
+  end
+
   def play
     display_welcome_message
     loop do
@@ -127,6 +145,8 @@ class RPSGame
       computer.choose
       display_moves
       display_winner
+      display_score
+      break if has_10_wins?
       break unless play_again?
     end
     display_goodbye_message
